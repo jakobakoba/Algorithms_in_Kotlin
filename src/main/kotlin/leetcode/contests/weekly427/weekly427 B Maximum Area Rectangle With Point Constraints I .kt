b83165b2d9658e4621c097b446367c73283c01b0
jakobakoba <borisgabyshev96dev@gmail.com>
2024-12-09 08:12:04 +0900

    задачи

--- src/main/kotlin/leetcode/contests/weekly427/weekly427 B Maximum Area Rectangle With Point Constraints I .kt
+++ src/main/kotlin/leetcode/contests/weekly427/weekly427 B Maximum Area Rectangle With Point Constraints I .kt
@@ -1,5 +1,7 @@
 package leetcode.contests.weekly427
 
+import kotlin.math.abs
+
 fun main(){
     class Solution {
         fun maxRectangleArea(points: Array<IntArray>): Int {
@@ -11,12 +13,13 @@ fun main(){
 
             var maxArea = -1
 
-
-            for (i in points.indices) {
-                for (j in i + 1 until points.size) {
+            val n = points.size
+            for (i in 0 until n) {
+                for (j in i + 1 until n) {
                     val (x1, y1) = points[i]
                     val (x2, y2) = points[j]
 
+                    val edges = setOf(Pair(x1,y1), Pair(x1,y2), Pair(x2,y1), Pair(x2,y2))
 
                     if (x1 != x2 && y1 != y2) {
 
@@ -25,10 +28,9 @@ fun main(){
                             var isValid = true
                             for (x in minOf(x1, x2) .. maxOf(x1, x2)) {
                                 for (y in minOf(y1, y2) .. maxOf(y1, y2)) {
-                                    if (Pair(x, y) in pointSet && Pair(x,y) != Pair(x1,y1) &&
-                                        Pair(x,y) != Pair(x1,y2) &&
-                                        Pair(x,y) != Pair(x2, y1) &&
-                                        Pair(x,y) != Pair(x2,y2)) {
+                                    // if it's in pointSet and it's not my 4 edges then it's gg
+                                    if (Pair(x, y) in pointSet
+                                        && Pair(x,y) !in edges) {
                                         isValid = false
                                         break
                                     }
@@ -36,19 +38,15 @@ fun main(){
                                 if (!isValid) break
                             }
 
-
                             if (isValid) {
-                                val area = kotlin.math.abs(x2 - x1) * kotlin.math.abs(y2 - y1)
+                                val area = abs(x2 - x1) * abs(y2 - y1)
                                 maxArea = maxOf(maxArea, area)
                             }
                         }
                     }
                 }
             }
-
-
             return maxArea
         }
     }
-
 }
